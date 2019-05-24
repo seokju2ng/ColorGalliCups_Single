b2d2b4bce7cd77d81e11cc2818c3506e1a1dfea8
ddochi93 <rangskim@naver.com>
2019-05-24 11:44:40 +0900

    Update Time1.java

--- src/view/etc/Time1.java
+++ src/view/etc/Time1.java
@@ -22,6 +22,7 @@ public class Time1 extends JPanel implements ActionListener {
 	/**남은시간을 나타내는 멤버이다.*/
 	private int sec;
 	/**전달받은 시간만큼 시간을 재주는 타이머를 생성한다. 전달받는 좌표와 크기로 Time1객체를 배치한다.
+	 * @param sec 타임어택 시간을 설정할 변수이다.
 	 * @param x Time1의 x 좌표이다.
 	 * @param y Time1의 y 좌표이다.
 	 * @param width Time1의 너비이다.
