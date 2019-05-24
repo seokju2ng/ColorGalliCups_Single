c59b2de1a5f50b9850a9a2ffc65c4592d16b091a
chlalstjd430 <50758600+chlalstjd430@users.noreply.github.com>
2019-05-24 10:52:28 +0900

    Update Key1pHandler.java
    update javadoc

--- src/view/Key1pHandler.java
+++ src/view/Key1pHandler.java
@@ -4,13 +4,22 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
 import javax.swing.JLabel;
-
+/**@author cms</br>
+ * 1P 색상키에 적용된다.색상키를 보여주는 UI(KeyControll, SinglePlayMode, DualPlayMode, NetworkPlayMode)에서 색상키에 해당하는 키(Q, W, E, A, S) Press시 
+색상 키 이미지를 눌린 이미지로 변경하고 키 release시 색상 키 이미지를 원상태로 변경한다.
+ * */
 public class Key1pHandler extends KeyAdapter{
+	/**색상키를 보여주는 UI에서 색상키에 해당하는 정보이다.*/
 	private JLabel[] labels;
-	
+	/**Key1pHandler의 생성자로 labels를 parameter로 받아 객체를 할당한다.
+	 * @param labels 색상키를 보여주는 UI에서 색상키에 해당하는 JLabel[]이다.
+	 * */
 	public Key1pHandler(JLabel[] labels) {
 		this.labels = labels;
 	}
+	/**색상키에 해당하는 키(Q, W, E, A, S)가 눌려질 때 이미지를 바꾸어주는 메소드
+	 * @param e 키 입력시 발생하는 이벤트이다.
+	 * */
 	public void keyPressed(KeyEvent e) {
 		int code = e.getKeyCode();
 
@@ -32,6 +41,9 @@ public class Key1pHandler extends KeyAdapter{
 			break;
 		}
 	}
+	/**색상키에 해당하는 키(Q, W, E, A, S)가 떼질 때 이미지를 바꾸어주는 메소드
+	 * @param e 키 입력시 발생하는 이벤트이다.
+	 * */
 	public void keyReleased(KeyEvent e) {
 		int code = e.getKeyCode();
 
