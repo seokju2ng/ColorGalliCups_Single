b43769ff2a492787af54f659c08338634aa5cb9b
DragonHee <50479743+DragonHee@users.noreply.github.com>
2019-05-24 11:01:00 +0900

    Add files via upload

--- src/model/CardSelectable.java
+++ src/model/CardSelectable.java
@@ -1,8 +1,21 @@
 package model;
 
 import java.util.ArrayList;
-
+/**
+ * 
+ * @author 김용희
+ * parameter로 받은 num만큼 카드를 뽑아 리턴 할 때, goldCard 한장을 뽑아 리턴할 때 implements하여 구현하여 사용 할 수 있다.
+ */
 public interface CardSelectable {
-	ArrayList<String[]> selectCards(int num);
-	String[] getGoldCard();
+	/**
+	 * parameter로 받은 num만큼 카드를  뽑아 리턴 한다. interface를 구현한 클래스에서 메소드 바디부를 작성하여 사용한다.
+	 * @param num 카드의 개수정보이다. num만큼 카드를 뽑아야 할 때 사용한다.
+	 * @return parameter로 받은 num만큼 카드를  뽑아 리턴 한다.
+	 */
+	ArrayList<Card> selectCards(int num);
+	/**
+	 * goldCard 한장을 뽑아 리턴한다. interface를 구현한 클래스에서 메소드 바디부를 작성하여 사용한다.
+	 * @return goldCard 한장을 뽑아 리턴한다.
+	 */
+	Card getGoldCard();
 }
