544468ad559ee6a5a9e850286abc829927e1ea14
chlalstjd430 <50758600+chlalstjd430@users.noreply.github.com>
2019-05-25 01:34:25 +0900

    Update CardSelectService.java
    update all method

--- src/model/CardSelectService.java
+++ src/model/CardSelectService.java
@@ -14,21 +14,40 @@ public class CardSelectService {
 	 * CardSelectService클래스의 Null Parameter Constructor이다.
 	 */
 	public CardSelectService() {
-		
+		cards = CardDao.getInstance();
 	}
 	/**
 	 * CardController로부터 요청을 받아 num만큼의 카드를 CardSelectable을 구현한 클래스에 요청하고 전달 받은 정보를 가공하여 CardController으로 리턴한다.
 	 * @param num 메소드 호출하는 측에서 리턴받고 싶어하는 카드의 장수에 해당되는 정보이다.
-	 * @return CardSelectable을 구현한 클래스로 부터 전달 받은 정보를 가공하여 CardController로 리턴한다.
+	 * @return CardSelectable 을 구현한 클래스로 부터 전달 받은 정보를 가공하여 CardController로 리턴한다.
 	 */
 	public ArrayList<String[]> selectCards(int num){
-		return null;
+		if(num < 0 || cards == null) return null;
+		
+		ArrayList<Card> originalCards = cards.selectCards(num);
+		ArrayList<String[]> selectCards = new ArrayList<String[]>();
+		
+		for(int i = 0; i < originalCards.size(); i++) {
+			String[] str = new String[3];
+			str[0] = originalCards.get(i).getNum() + "";
+			str[1] = originalCards.get(i).getPath();
+			str[2] = originalCards.get(i).getAnswer();
+			selectCards.add(str);
+		}
+		return selectCards;
 	}
 	/**
 	 * CardController로부터 요청을 받아 goldCard를 CardSelectable을 구현한 클래스에 요청하여 전달 받은 정보를 가공하여 CardController로 리턴한다.
 	 * @return CardSelectable을 구현한 클래스로 부터 전달 받은 정보를 가공하여 CardController로 리턴한다.
 	 */
 	public String[] getGoldCard() {
-		return null;
+		if(cards == null) return null;
+		
+		Card card = cards.getGoldCard();
+		String[] str = new String[3];
+		str[0] = card.getNum()+"";
+		str[1] = card.getPath();
+		str[2] = card.getAnswer();
+		return str;
 	}
 }
