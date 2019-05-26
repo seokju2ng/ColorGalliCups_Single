9d63514f9255c38b33e1c4dc91f87efcb091875b
chlalstjd430 <50758600+chlalstjd430@users.noreply.github.com>
2019-05-26 21:47:01 +0900

    Update Sound.java
    edit All

--- src/view/etc/Sound.java
+++ src/view/etc/Sound.java
@@ -11,15 +11,43 @@ import javax.sound.sampled.Clip;
 public class Sound {
 		/**파일의 경로를 받아 해당 파일의 소리를 재생해주는 메소드이다.
 		 * @param fileName 게임에서 사용 하고싶은 소리의 파일 경로이다.*/
-	   public static void playSound(String fileName) {
+		private boolean effectFlag;
+		private boolean bgmFlag;
+		public static AudioInputStream ais;
+		private static Clip BgmClip;
+		
+		public static void playEffect(String fileName) {
+		//if(effectFlag == true)
 	      try {
-	         AudioInputStream ais = AudioSystem.getAudioInputStream(new File(fileName));
+	    	 AudioInputStream ais = AudioSystem.getAudioInputStream(new File(fileName));
 	         Clip clip = AudioSystem.getClip();
 	         clip.stop();
 	         clip.open(ais);
 	         clip.start();
-	      } catch (Exception e) {
+
+	      }  catch (Exception e) {
 	         e.printStackTrace();
 	      }
 	   }
+		public static void playBgm(String fileName) {
+			//if(bgmFlag == true) 
+		      try {
+		    	 ais = AudioSystem.getAudioInputStream(new File(fileName));
+		    	 BgmClip = AudioSystem.getClip();
+		    	 BgmClip.stop();
+		    	 BgmClip.open(ais);
+		    	 BgmClip.start();
+		    	 BgmClip.loop(-1);
+		      } catch (Exception e) {
+		         e.printStackTrace();
+		      }
+		   }
+	   public static void stop() {
+		      try {
+		    	  BgmClip.stop();
+
+		      } catch (Exception e) {
+		         e.printStackTrace();
+		      }
+		   }
 }
