c92e835009a3e80c73fcfa98053e70b73bc8b29f
seokju2ng <43347250+seokju2ng@users.noreply.github.com>
2019-05-26 17:27:39 +0900

    Add files via upload

--- src/view/bean/Ranks.java
+++ src/view/bean/Ranks.java
@@ -5,23 +5,23 @@ import java.util.ArrayList;
 import controller.RankController;
 
 /**
- * RankBean의 정보들을 Collection 타입으로 관리하는 클래스이다. CRUD 메소드를 가지고 있으며 RankBean에 관련된
- * Service에서 사용할 랭크정보들을 저장하는 클래스이다.
+ * View RankBean의 객체들을 Collection 타입으로 관리하는 클래스이다.
+ * RankBean에 관련된 Service에서 사용할 랭크정보들을 저장하는 클래스이다.
  * 
- * @author 송준희
+ * @author seokjung
  */
 public class Ranks {
 	/**
-	 * RankBean의 정보들을 저장하고 있다.
+	 * 랭크 정보를 리스트로 가지고 있다.
 	 */
 	private ArrayList<RankBean> ranks;
 	/**
-	 * Ranks의 서비스로부터 받은 랭킹정보들을 SinglePlayMode 객체가 사용할 수 있는 형태로 가공하여 전달해준다.
+	 * Ranks의 서비스로부터 받은 랭킹정보들을 SinglePlayMode 객체가 사용할 수 있는 형태로 가공하여 전달해주는 컨트롤러 객체이다.
 	 */
 	private RankController rankController;
 
 	/**
-	 * null-parameter Constructor
+	 * Null-Parameter Constructor : RankController를 통해 랭킹 정보를 담고 있는 File에서 랭킹정보를 읽어와 저장한다.
 	 */
 	public Ranks() {
 		// ranks = new ArrayList<>();
@@ -30,12 +30,10 @@ public class Ranks {
 	}
 
 	/**
-	 * 사용자로부터 RankBean에 저장할 값들을 받아 RankBean 객체를 생성하고 ArrayList에 삽입하는 insert() 메소드이다.
+	 * 랭킹에 저장할 정보를 전달 받아 RankController를 통해 랭킹 삽입을 하고 파일에 저장한다.
 	 * 
-	 * @param name
-	 *            ArrayList의 RankBean에 저장할 사용자의 name에 해당하는 값이다.
-	 * @param score
-	 *            ArrayList의 RankBean에 저장할 사용자의 score에 해당하는 값이다.
+	 * @param name 랭킹에 삽입될 이름이다.
+	 * @param score 랭킹에 삽입될 점수이다.
 	 * @return 삽입에 성공하면 true, 아니면 false를 반환한다.
 	 */
 	public boolean insert(String name, int score) {
@@ -60,8 +58,7 @@ public class Ranks {
 	/**
 	 * 사용자로부터 검색할 점수를 받아 Ranks에서 해당하는 점수를 찾는 search() 메소드이다.
 	 * 
-	 * @param score
-	 *            사용자가 Ranks에서 찾을 점수이다.
+	 * @param score 사용자가 Ranks에서 찾을 점수이다.
 	 * @return 해당하는 점수가 들어있는 ArrayList의 index를 반환한다. 실패시 -1반환.
 	 */
 	public int search(int score) {
@@ -100,12 +97,12 @@ public class Ranks {
 		return ranks;
 	}
 	/**
-	 * 사용자의 점수가 랭킹 5위안에 드는지 확인해주는 메소드이다.
-	 * @param score 사용자가 받은 점수이다.
-	 * @return 사용자의 점수가 5위안에 들면 true, 아니면 false를 리턴한다.
+	 * 점수를 전달 받아 해당 점수가 5위 안에 드는지 체크해주는 메서드이다. 
+	 * @param score 5위 안에 드는지 체크할 점수이다.
+	 * @return 전달받은 점수가 5위 안에 드는 점수이면 true, 아니면 false를 반환한다.
 	 */
 	public boolean isRanker(int score) {
-		if (ranks == null) {
+		if (ranks == null || score == 0) {
 			return false;
 		}
 		if (ranks.size() < 5) // 기존 랭커가 5명미만 이면 무조건 랭커가 된다.
