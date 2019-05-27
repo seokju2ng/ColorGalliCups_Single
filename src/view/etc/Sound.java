57900617fc437eb4c7173b7cbc804b95307b86b0
chlalstjd430 <50758600+chlalstjd430@users.noreply.github.com>
2019-05-27 21:29:27 +0900

    Update Sound.java
    add Member

--- src/view/etc/Sound.java
+++ src/view/etc/Sound.java
@@ -5,49 +5,71 @@ import java.io.File;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+
 /**
  * 게임에서 사용되는 모든 소리를 관리한다.
- * @author cms<br>*/
+ * 
+ * @author cms<br>
+ */
 public class Sound {
-		/**파일의 경로를 받아 해당 파일의 소리를 재생해주는 메소드이다.
-		 * @param fileName 게임에서 사용 하고싶은 소리의 파일 경로이다.*/
-		private boolean effect;
-		private boolean bgm;
-		private static AudioInputStream ais;
-		private static Clip bgmClip;
-		
-		public static void playEffect(String fileName) {
-		//if(effectFlag == true)
-	      try {
-	    	 AudioInputStream ais = AudioSystem.getAudioInputStream(new File(fileName));
-	         Clip clip = AudioSystem.getClip();
-	         clip.stop();
-	         clip.open(ais);
-	         clip.start();
+	/**
+	 * 파일의 경로를 받아 해당 파일의 소리를 재생해주는 메소드이다.
+	 * 
+	 * @param fileName 게임에서 사용 하고싶은 소리의 파일 경로이다.
+	 */
+	private static boolean effect;
+	private static boolean bgm;
+	private static AudioInputStream ais;
+	private static Clip bgmClip;
 
-	      }  catch (Exception e) {
-	         e.printStackTrace();
-	      }
-	   }
-		public static void playBgm(String fileName) {
-			//if(bgmFlag == true) 
-		      try {
-			    	 ais = AudioSystem.getAudioInputStream(new File(fileName));
-			    	 bgmClip = AudioSystem.getClip();
-			    	 bgmClip.stop();
-			    	 bgmClip.open(ais);
-			    	 bgmClip.start();
-			    	 bgmClip.loop(-1);
-		      } catch (Exception e) {
-		         e.printStackTrace();
-		      }
-		   }
-	   public static void stop() {
-		      try {
-		    	  bgmClip.stop();
+	public static void playEffect(String fileName) {
+		// if(effectFlag == true)
+		try {
+			AudioInputStream ais = AudioSystem.getAudioInputStream(new File(fileName));
+			Clip clip = AudioSystem.getClip();
+			clip.stop();
+			clip.open(ais);
+			clip.start();
 
-		      } catch (Exception e) {
-		         e.printStackTrace();
-		      }
-		   }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void playBgm(String fileName) {
+		// if(bgmFlag == true)
+		try {
+			ais = AudioSystem.getAudioInputStream(new File(fileName));
+			bgmClip = AudioSystem.getClip();
+			bgmClip.stop();
+			bgmClip.open(ais);
+			bgmClip.start();
+			bgmClip.loop(-1);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void stop() {
+		try {
+			bgmClip.stop();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void bgmOn() {
+		bgm = true;
+	}
+	public static void bgmOff() {
+		bgm = false;
+	}
+	public static void effectOn() {
+		effect = true;
+	}
+	public static void effectOff() {
+		effect = false;
+	}
+
 }
