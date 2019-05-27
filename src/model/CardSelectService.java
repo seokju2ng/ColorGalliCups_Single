9bb312762283230333a9972ca2c58eaf0db08e7a
ddochi93 <rangskim@naver.com>
2019-05-27 22:47:56 +0900

    Update CardSelectService.java

--- src/model/CardSelectService.java
+++ src/model/CardSelectService.java
@@ -5,11 +5,11 @@ import java.util.ArrayList;
  * CardDao에게 요청한 카드 개수만큼 카드덱을 받아 CardController에게 전달해준다.
  * @author 김용희
  */
-public class CardSelectService {
+public class CardSelectService implements CardSelectable{
 	/**
 	 * CardSelectable 타입의 변수이다.
 	 */
-	private CardSelectable cards;
+	private CardDao cards;
 	/**
 	 * CardSelectService클래스의 Null Parameter Constructor이다.
 	 */
@@ -21,6 +21,7 @@ public class CardSelectService {
 	 * @param num 메소드 호출하는 측에서 리턴받고 싶어하는 카드의 장수에 해당되는 정보이다.
 	 * @return CardSelectable 을 구현한 클래스로 부터 전달 받은 정보를 가공하여 CardController로 리턴한다.
 	 */
+	@Override
 	public ArrayList<String[]> selectCards(int num){
 		if(num < 0 || cards == null) return null;
 		
@@ -40,6 +41,7 @@ public class CardSelectService {
 	 * CardController로부터 요청을 받아 goldCard를 CardSelectable을 구현한 클래스에 요청하여 전달 받은 정보를 가공하여 CardController로 리턴한다.
 	 * @return CardSelectable을 구현한 클래스로 부터 전달 받은 정보를 가공하여 CardController로 리턴한다.
 	 */
+	@Override
 	public String[] getGoldCard() {
 		if(cards == null) return null;
 		
@@ -50,4 +52,22 @@ public class CardSelectService {
 		str[2] = card.getAnswer();
 		return str;
 	}
+	//0527 Edit By DK KIM//
+	@Override
+	public ArrayList<String[]> getCards() {
+		if(cards == null) return null;
+		ArrayList<Card> originalCards = cards.getCards();
+		ArrayList<String[]> selectCards = new ArrayList<String[]>();
+		
+		for(int i = 0; i < originalCards.size(); i++) {
+			String[] str = new String[3];
+			str[0] = originalCards.get(i).getNum() + "";
+			str[1] = originalCards.get(i).getPath();
+			str[2] = originalCards.get(i).getAnswer();
+			selectCards.add(str);
+		}
+		return selectCards;
+		
+	}
+
 }
