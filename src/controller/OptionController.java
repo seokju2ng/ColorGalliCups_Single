5cf1f737f5985a558f962be1770d65995e015669
seokju2ng <43347250+seokju2ng@users.noreply.github.com>
2019-05-27 23:05:51 +0900

    option update

--- src/controller/OptionController.java
+++ src/controller/OptionController.java
@@ -14,51 +14,27 @@ public class OptionController {
 		if(optionService == null) return null;
 		ArrayList<String> option = optionService.getOption();
 		boolean bgm = option.get(0).equals("ON") ? true : false;
-		boolean sound = option.get(1).equals("ON") ? true : false;
+		boolean effect = option.get(1).equals("ON") ? true : false;
 		int cardNum = Integer.parseInt(option.get(2));
-		return new OptionBean(bgm, sound, cardNum);
+		return new OptionBean(bgm, effect, cardNum);
 	}
 
 	public void setOption(OptionBean option) {
 		if(optionService == null || option == null) return;
 		ArrayList<String> op = new ArrayList<String>();
 		op.add(option.isBgm()?"ON":"OFF");
-		op.add(option.isSound()?"ON":"OFF");
+		op.add(option.isEffect()?"ON":"OFF");
 		op.add(option.getCardNum()+"");
 		optionService.setOption(op);
 	}
 	
-	public void setOption(boolean bgm, boolean sound, int cardNum) {
+	public void setOption(boolean bgm, boolean effect, int cardNum) {
 		if(optionService == null) return;
-		optionService.setOption(bgm, sound, cardNum);
+		optionService.setOption(bgm, effect, cardNum);
 	}
 	
-//	public void bgmOn() {
-//		if(optionService == null) return;
-//		optionService.bgmOn();
-//	}
-//	public void bgmOff() {
-//		if(optionService == null) return;
-//		optionService.bgmOff();
-//	}
-//	public void soundOn() {
-//		if(optionService == null) return;
-//		optionService.soundOn();
-//	}
-//	public void soundOff() {
-//		if(optionService == null) return;
-//		optionService.soundOff();
-//	}
-//	public void setCardNum(int cardNum) {
-//		if(optionService == null) return;
-//		optionService.setCardNum(cardNum);
-//	}
-	
-//	public boolean loadOption() {
-//		return optionService.loadOption();
-//	}
-	
 	public boolean saveOption() {
+		if(optionService == null) return false;
 		return optionService.saveOption();
 	}
 }
