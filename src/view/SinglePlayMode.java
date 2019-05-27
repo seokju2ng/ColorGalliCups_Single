306b6f07147d90f6dc87e4d8f65048140cf5c813
DragonHee <50479743+DragonHee@users.noreply.github.com>
2019-05-27 22:46:11 +0900

    Update SinglePlayMode.java

--- src/view/SinglePlayMode.java
+++ src/view/SinglePlayMode.java
@@ -125,7 +125,8 @@ public class SinglePlayMode extends JPanel implements ActionListener {
       this.setFocusTraversalKeysEnabled(false);
       this.addComponentListener(new FocusHandler());
       // 카드를 가장 먼저 붙음 Edit by DK KIM//
-      cardDeck = new CardDeck().shuffle();
+      cardDeck = new CardDeck();
+      cardDeck.shuffle();
       one_Deck = new ArrayList<>();
 
       cardDeckLabels = new ArrayList<>();
