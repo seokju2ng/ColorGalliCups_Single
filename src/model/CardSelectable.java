32ad3d13b1e23e3fd40116355431dc57985973d7
ddochi93 <rangskim@naver.com>
2019-05-27 22:47:12 +0900

    Update CardSelectable.java

--- src/model/CardSelectable.java
+++ src/model/CardSelectable.java
@@ -11,10 +11,13 @@ public interface CardSelectable {
 	 * @param num 카드의 개수정보이다. num만큼 카드를 뽑아야 할 때 사용한다.
 	 * @return parameter로 받은 num만큼 카드를  뽑아 리턴 한다.
 	 */
-	ArrayList<Card> selectCards(int num);
+	public ArrayList<String[]> selectCards(int num);
+	
+	//0527 EDIT BY DK KIM//
+	public ArrayList<String[]> getCards();
 	/**
 	 * goldCard 한장을 뽑아 리턴한다. interface를 구현한 클래스에서 메소드 바디부를 작성하여 사용한다.
 	 * @return goldCard 한장을 뽑아 리턴한다.
 	 */
-	Card getGoldCard();
+	public String[] getGoldCard();
 }
