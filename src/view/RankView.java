6ad8c14ec01b61d798c6f8f91fe245a4367c392d
seokju2ng <43347250+seokju2ng@users.noreply.github.com>
2019-05-28 18:00:18 +0900

    Update RankView.java

--- src/view/RankView.java
+++ src/view/RankView.java
@@ -1,139 +1,165 @@
-package view.bean;
+package view;
 
-import java.util.ArrayList;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
-import controller.RankController;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
+import view.bean.RankBean;
+import view.bean.Ranks;
+import view.etc.ChangePanelService;
 /**
- * View RankBean의 객체들을 Collection 타입으로 관리하는 클래스이다. RankBean에 관련된 Service에서 사용할
- * 랭크정보들을 저장하는 클래스이다.
- * 
+ * 랭크 리스트를 팝업으로 보여주는 RankView 클래스이다. 랭크 리스트에는 순위와 이름, 점수가 등록되어 있다.
  * @author seokjung
+ *
  */
-public class Ranks {
+public class RankView extends JDialog {
 	/**
-	 * 랭크 정보를 리스트로 가지고 있다.
+	 * 랭크 리스트 정보를 RankController를 통해 받아와 저장하는 view bean의 컬렉션 객체이다.
 	 */
-	private ArrayList<RankBean> ranks;
+	private Ranks r;
 	/**
-	 * Ranks의 서비스로부터 받은 랭킹정보들을 SinglePlayMode 객체가 사용할 수 있는 형태로 가공하여 전달해주는 컨트롤러 객체이다.
+	 * 랭크 정보를 view에서 보여줄 JLabel 배열이다.
+	 * 한 행에는 순위, 이름, 점수가 JLabel 텍스트로 가지고 있다.
 	 */
-	private RankController rankController;
+	private JLabel[][] ranks;
 	/**
-	 * Ranks 객체를 SingleTone으로 생성해주기 위해 접근 제어자를 static으로 설정하고 Ranks를 저장하고 있다.
+	 * bgm을 바꿔야하는지 안바꿔도 되는지 체크할 flag 변수이다.
 	 */
-	private static Ranks instance;
+	private boolean bgmFlag;
+	
+	/* 
+	 * Edit by Seokju2ng 0525
+	 * */
+	
+	/**
+	 * Null-Parameter Constructor : MainView에서 RankView에 접근했을 때 팝업창을 보여주는 생성자이다.
+	 * Ranks 객체로 RankController를 통해 파일에 저장된 랭크 정보를 불러온다. 
+	 */
+	public RankView() {
+		super.setTitle("Rank");
+		setLayout(null);
 
-	/**
-	 * Null-Parameter Constructor : RankController를 통해 랭킹 정보를 담고 있는 File에서 랭킹정보를 읽어와
-	 * 저장한다.
-	 */
-	private Ranks() {
-		rankController = new RankController();
-		ranks = rankController.getRanks();
-	}
-	/**
-	 * 랭킹 정보가 추가될 때 불리는 메서드이다. 랭킹에 추가될 index, 랭킹, 이름, 점수가 파라메터로 전달되면 해당 index에 삽입한다.
-	 * @param index 랭킹정보에 추가될 index를 전달하는 변수이다.
-	 * @param ranking 랭킹정보에 추가될 랭킹을 전달하는 변수이다.
-	 * @param name 랭킹정보에 추가될 이름을 전달하는 변수이다.
-	 * @param score 랭킹정보에 추가될 점수를 전달하는 변수이다.
-	 * @return 삽입에 성공하면 true, 아니면 false를 반환한다.
-	 */
-	public boolean insert(int index, int ranking, String name, int score) {
-		ranks.add(index, new RankBean(ranking, name, score));	
-		int length = ranks.size();
-		for(int i = index + 1; i < length ; i++) {
-			ranks.get(i).setRanking(ranks.get(i).getRanking() + 1);
-		}
-		if(ranks.size() > 5) ranks.remove(5);
-		return true;
-	}
-	/**
-	 * 랭킹에 저장할 정보를 전달 받아 RankController를 통해 랭킹 삽입을 하고 파일에 저장한다.
-	 * 
-	 * @param name 랭킹에 삽입될 이름이다.
-	 * @param score 랭킹에 삽입될 점수이다.
-	 * @return 삽입에 성공하면 true, 아니면 false를 반환한다.
-	 */
-	public boolean insertRank(String name, int score) {
-		String[] rankInfo = rankController.insertRank(name, score);
+		r = Ranks.getInstance();
+		bgmFlag = false;
 		
-		int index = Integer.parseInt(rankInfo[0]);
-		int ranking = Integer.parseInt(rankInfo[1]);
-				
-		return insert(index, ranking, name, score);
-	}
+//		Font cfont = new Font("배달의민족 한나체 Pro", Font.BOLD, 30);
+		Font font = new Font("배달의민족 한나체 Pro", Font.PLAIN, 20);
 
-	/**
-	 * 사용자로부터 검색할 점수를 받아 Ranks에서 해당하는 점수를 찾는 search() 메소드이다.
-	 * 
-	 * @param score
-	 *            사용자가 Ranks에서 찾을 점수이다.
-	 * @return 해당하는 점수가 들어있는 ArrayList의 index를 반환한다. 실패시 -1반환.
-	 */
-	public int search(int score) {
-		int idx = -1;
-		for (int i = 0; i < ranks.size(); i++) {
-			if (score == ranks.get(i).getScore()) {
-				idx = i;
-				return idx;
+		JPanel panel = new JPanel(new GridLayout(0, 3, 0, 0));
+		panel.setBackground(new Color(250, 250, 250));
+		panel.setBounds(100, 100, 400, 400);
+
+		JLabel[] cols = new JLabel[] { new JLabel("순위", SwingConstants.CENTER), new JLabel("이름", SwingConstants.CENTER),
+				new JLabel("점수", SwingConstants.CENTER) };
+		
+		ranks = new JLabel[5][3];
+		
+		panel.add(cols[0]);	panel.add(cols[1]);	panel.add(cols[2]);
+		for(int i = 0; i < ranks.length; i++) {
+			ranks[i][0] = new JLabel("", SwingConstants.CENTER);
+			ranks[i][1] = new JLabel("", SwingConstants.CENTER);
+			ranks[i][2] = new JLabel("", SwingConstants.CENTER);
+			for(int j = 0; j < 3; j++) {
+				ranks[i][j].setFont(font);
+				panel.add(ranks[i][j]);
 			}
 		}
-		return idx;
-	}
+		
+		for(int i = 0; i < r.getRanks().size(); i++) {
+			RankBean rank = r.getRanks().get(i);
+			ranks[i][0].setText(rank.getRanking()+"");
+			ranks[i][1].setText(rank.getName());
+			ranks[i][2].setText(rank.getScore()+"");
+			
+		}
+		panel.setBounds(40, 25, 600, 300);
+		add(panel, BorderLayout.CENTER);
 
+		// buttons: 초기화 버튼, 확인 버튼
+		JButton reset = new JButton("초기화");
+		JButton check = new JButton("확인");
+
+		JPanel panel2 = new JPanel(new FlowLayout(FlowLayout.CENTER, 80, 20));
+		reset.setContentAreaFilled(false);
+		reset.setFocusPainted(false);
+		reset.setBorderPainted(false);
+		check.setContentAreaFilled(false);
+		check.setFocusPainted(false);
+		check.setBorderPainted(false);
+		reset.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int result = JOptionPane.showConfirmDialog(null, "정말로 초기화하시겠습니까?", "랭크 초기화", JOptionPane.YES_NO_OPTION,
+						JOptionPane.WARNING_MESSAGE);
+				if (result == JOptionPane.YES_OPTION) {
+					if(r.clearRanking()) {
+						for(int i = 0; i < ranks.length; i++) {
+							ranks[i][0].setText("");
+							ranks[i][1].setText("");
+							ranks[i][2].setText("");
+						}
+					}
+				}
+			}
+		});
+		check.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+				if(bgmFlag)
+					ChangePanelService.getInstance().changePanel("MainView", null); //확인 버튼 누르면 메인뷰로 돌아간다.
+				else ChangePanelService.getInstance().changePanel("MainView");
+			}
+		});
+		this.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				if(bgmFlag)
+					ChangePanelService.getInstance().changePanel("MainView", null); //확인 버튼 누르면 메인뷰로 돌아간다.
+				else ChangePanelService.getInstance().changePanel("MainView");
+			}
+		});
+
+		panel2.add(reset);
+		panel2.add(check);
+
+		panel2.setBounds(40, 320, 600, 75);
+		add(panel2, BorderLayout.SOUTH);
+
+		setSize(700, 450);
+		setVisible(true);
+		// setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+	}
 	/**
-	 * Ranks의 ArrayList에 있는 모든 RankBean 객체를 제거하는 메소드이다.
-	 * 
-	 * @return 초기화에 성공하면 true, 실패하면 false를 반환한다.
+	 * Overloaded Constructor : SinglePlayMode에서 RankView에 접근했을 때 팝업창을 보여주는 생성자이다.
+	 * 싱글 게임이 종료했을 때 랭크 5위 안에 들었다면, 파라메터를 통해 삽입할 랭킹정보를 받아와 랭킹에 삽입한다.
+	 * 삽입을 마친 후 Ranks 객체로 RankController를 통해 파일에 저장된 랭크 정보를 불러온다.
+	 * @param name 랭킹 정보에 삽입될 이름이다.
+	 * @param score 랭킹 정보에 삽입될 점수이다.
 	 */
-	public boolean clearRanking() {
-		// if (ranks == null) {
-		// System.out.println("ranks가 null입니다!");
-		// return false;
-		// }
-		// ranks.clear();
-		boolean res = rankController.clearRanking();
-		if(res) {
-			ranks.clear();
+	public RankView(String name,int score) {
+		this();
+		bgmFlag = true;
+		r.insertRank(name, score);
+//		if(r.insertRank(name, score)) {
+//			r = Ranks.getInstance();
+//		}
+		int n = r.getRanks().size();
+		for(int i = 0; i < n; i++) {
+			RankBean rank = r.getRanks().get(i);
+			ranks[i][0].setText(rank.getRanking()+"");
+			ranks[i][1].setText(rank.getName());
+			ranks[i][2].setText(rank.getScore()+"");
 		}
-		return res;
-	}
-
-	/**
-	 * Ranks 클래스에서 Ranks 필드의 참조값을 리턴해주는 getter() 메소드이다.
-	 * 
-	 * @return Ranks필드의 참조값을 리턴해준다.
-	 */
-	public ArrayList<RankBean> getRanks() {
-		return ranks;
-	}
-
-	/**
-	 * 점수를 전달 받아 해당 점수가 5위 안에 드는지 체크해주는 메서드이다.
-	 * 
-	 * @param score
-	 *            5위 안에 드는지 체크할 점수이다.
-	 * @return 전달받은 점수가 5위 안에 드는 점수이면 true, 아니면 false를 반환한다.
-	 */
-	public boolean isRanker(int score) {
-		if (ranks == null || score == 0) {
-			return false;
-		}
-		if (ranks.size() < 5) // 기존 랭커가 5명미만 이면 무조건 랭커가 된다.
-			return true;
-
-		if (this.ranks.get(4).getScore() < score) {
-			return true;
-		}
-		return false;
-	}
-
-	public static Ranks getInstance() {
-		if (instance == null) {
-			instance = new Ranks();
-		}
-		return instance;
 	}
 }
