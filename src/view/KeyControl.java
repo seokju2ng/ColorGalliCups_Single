ff489fbf007254e553a2234e06d6c2924afc55db
mornings9047 <50758526+mornings9047@users.noreply.github.com>
2019-05-28 17:38:32 +0900

    Update KeyControl.java
    new BackHelpHandler() 두번 해서 한번만 생성하도록 수정함

--- src/view/KeyControl.java
+++ src/view/KeyControl.java
@@ -5,22 +5,18 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-import view.etc.ChangePanelService;
 import view.etc.KeyImage;
 import view.handler.BackHelpHandler;
 import view.handler.FocusHandler;
 
 public class KeyControl extends JPanel {
+	private static final long serialVersionUID = 1L;
 	private JLabel[] soloKey;
 	private JLabel[] dualKey1;
 	private JLabel[] dualKey2;
@@ -29,13 +25,9 @@ public class KeyControl extends JPanel {
 		this.setLayout(null);
 		this.setBackground(new Color(251, 229, 214));
 		this.addComponentListener(new FocusHandler());
-		this.addKeyListener(new BackHelpHandler());
+		BackHelpHandler l = new BackHelpHandler();
+		this.addKeyListener(l);
 
-		// JButton[] threeButtons=new JButton[] {new JButton(new
-		// ImageIcon("image/red(q)_k.png")), new JButton(new
-		// ImageIcon("image/green(w)_k.png")), new JButton(new
-		// ImageIcon("image/blue(e)_k.png"))};
-		
 		soloKey = KeyImage.getKey("1P", 90, 90);
 		JLabel functionButton = new JLabel(new ImageIcon("image/spacebar.png"));
 		JLabel singlePlay = new JLabel(new ImageIcon("image/singleplay.png"));
@@ -50,12 +42,6 @@ public class KeyControl extends JPanel {
 		}
 		functionButton.setBounds(190, 350, 350, 94);
 		add(functionButton);
-		///////////////////////////////////////////// SINGLEPLAY
-		///////////////////////////////////////////// 끝////////////////////////////////////////////////////////
-
-		///////////////////////////////////////////// DOUBLEPLAY
-		///////////////////////////////////////////// /////////////////////////////////////////////////////////
-
 		JLabel doublePlay = new JLabel(new ImageIcon("image/doubleplay.png"));
 
 		doublePlay.setBounds(610, 10, 250, 150);
@@ -68,21 +54,6 @@ public class KeyControl extends JPanel {
 			player[i].setFont(font);
 			add(player[i]);
 		}
-		// JLabel pp=new JLabel(new ImageIcon("image/1p2p.png"));
-		// pp.setBounds(790, 140, 350, 100);
-		// add(pp);
-
-		// JButton[] firstPlay3=new JButton[] {new JButton(new
-		// ImageIcon("image/red(q)_1p_k.png")), new JButton(new
-		// ImageIcon("image/green(w)_1p_k.png")), new JButton(new
-		// ImageIcon("image/blue(e)_1p_k.png"))};
-		// for(int i=0;i<firstPlay3.length;i++) {
-		// firstPlay3[i].setBounds(730+70*i, 230, 60, 60);
-		// firstPlay3[i].setContentAreaFilled(false);
-		// firstPlay3[i].setFocusPainted(false);
-		// firstPlay3[i].setBorderPainted(false);
-		// add(firstPlay3[i]);
-		// }
 
 		dualKey1 = KeyImage.getKey("1P", 60, 60);
 		JLabel functionButton1 = new JLabel(new ImageIcon("image/spacebar_1p.png"));
@@ -102,17 +73,6 @@ public class KeyControl extends JPanel {
 		functionButton1.setBounds(735, 400, 191, 59);
 		add(functionButton1);
 
-		// JButton[] secondPlay3=new JButton[] {new JButton(new
-		// ImageIcon("image/red(i)_2p_k.png")), new JButton(new
-		// ImageIcon("image/green(o)_2p_k.png")), new JButton(new
-		// ImageIcon("image/blue(p)_2p_k.png"))};
-		// for(int i=0;i<threeButtons.length;i++) {
-		// secondPlay3[i].setBounds(1010+70*i, 230, 60, 60);
-		// secondPlay3[i].setContentAreaFilled(false);
-		// secondPlay3[i].setFocusPainted(false);
-		// secondPlay3[i].setBorderPainted(false);
-		// add(secondPlay3[i]);
-		// }
 		dualKey2 = KeyImage.getKey("2P", 60, 60);
 		JLabel functionButton2 = new JLabel(new ImageIcon("image/enter_2p.png"));
 
@@ -132,24 +92,6 @@ public class KeyControl extends JPanel {
 		functionButton2.setBounds(1015, 400, 191, 60);
 		add(functionButton2);
 
-		///////////////////////////////////////////// DOUBLEPLAY 끝
-		///////////////////////////////////////////// /////////////////////////////////////////////////////////
-		// Font font=new Font("Nanum Brush Script", Font.BOLD, 45);
-		// JLabel threeColors=new JLabel("3색");
-		// add(threeColors);
-		// threeColors.setBounds(630,230,100,50);
-		// threeColors.setFont(font);
-		// threeColors.setForeground(new Color(197, 90, 17));
-		//
-		// JLabel fiveColors=new JLabel("5색");
-		// add(fiveColors);
-		// fiveColors.setBounds(630,380,100,50);
-		// fiveColors.setFont(font);
-		// fiveColors.setForeground(new Color(197, 90, 17));
-
-		///////////////////////////////////////////// 뒤로가기
-		///////////////////////////////////////////// /////////////////////////////////////////////////////////////////
-		BackHelpHandler l = new BackHelpHandler();
 		JButton back = new JButton("뒤로가기");
 		back.setFont(font);
 		back.setForeground(new Color(197, 90, 17));
@@ -163,15 +105,13 @@ public class KeyControl extends JPanel {
 		this.addKeyListener(new Key1pHandler(soloKey));
 		this.addKeyListener(new Key1pHandler(dualKey1));
 		this.addKeyListener(new Key2pHandler(dualKey2));
-		
+
 		JLabel rarrow = new JLabel(new ImageIcon("image/right.png"));
 		JLabel larrow = new JLabel(new ImageIcon("image/left.png"));
 		larrow.setBounds(470, 540, 100, 100);
 		rarrow.setBounds(765, 540, 100, 100);
 		add(rarrow);
 		add(larrow);
-		///////////////////////////////////////////// 뒤로가기 끝
-		///////////////////////////////////////////// /////////////////////////////////////////////////////////
 	}
 
 	public void paintComponent(Graphics g) {
@@ -182,5 +122,4 @@ public class KeyControl extends JPanel {
 		g2.drawRoundRect(80, 110, 550, 400, 50, 50);
 		g2.drawRoundRect(700, 110, 550, 400, 50, 50);
 	}
-
 }
