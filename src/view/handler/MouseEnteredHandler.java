805b6abadc56cd4400d611cec98910827e7cb592
ddochi93 <rangskim@naver.com>
2019-05-29 10:00:14 +0900

    Update MouseEnteredHandler.java

--- src/view/handler/MouseEnteredHandler.java
+++ src/view/handler/MouseEnteredHandler.java
@@ -6,7 +6,7 @@ import java.awt.event.MouseEvent;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 
-import view.MyIndex;
+import view.etc.MyIndex;
 import view.etc.Sound;
 /**
  * 메뉴 선택 UI(MainView, GameMode, Help)에서 메뉴 버튼에 마우스 진입 시 현재 메뉴를 표시하는 아이콘의 위치를 마우스 위치에 해당하는 메뉴로 변경한다.
