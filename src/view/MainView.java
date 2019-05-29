e7b29f10eb9a68f32c670871897f9b3fdffcf057
seokju2ng <43347250+seokju2ng@users.noreply.github.com>
2019-05-29 15:26:17 +0900

    Update MainView.java

--- src/view/MainView.java
+++ src/view/MainView.java
@@ -43,6 +43,7 @@ public class MainView extends JPanel {
 	public MainView() {
 		cor = new MyIndex();
 		this.addComponentListener(new FocusHandler());
+		this.setFocusTraversalKeysEnabled(false);
 		setLayout(new BorderLayout());
 		makeUI();
 	}
